30d315b8d7c65b82963619d0512cc73f9711ffae
Obumezi <eziamakaobum@yahoo.co.uk>
2022-04-04 21:50:21 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+
+
+export function Header() {
+    return (
+        <div className="meme-header">
+            <div className="left-side">
+            
+            <h1>Meme Generator</h1>
+            </div>
+            <div className="right-side">
+            <p>Project 3</p>
+            </div>
+            
+        </div>
+
+    )
+
+}
