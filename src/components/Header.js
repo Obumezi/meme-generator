30546eabea40f22af99baf29a23da5924d6d5c31
Obumezi <eziamakaobum@yahoo.co.uk>
2022-04-14 22:49:47 +0100

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
-
+import meme from "../assets/troll-face.svg"
 
 export function Header() {
     return (
-        <div className="meme-header">
+        <div className="meme-header" alt="meme face">
             <div className="left-side">
-            
-            <h1>Meme Generator</h1>
+                <img src={meme} alt="meme-face" className="meme-image" />
+
+                <h1>Meme Generator</h1>
             </div>
             <div className="right-side">
-            <p>Project 3</p>
+                <p>Project 3</p>
             </div>
-            
+
         </div>
 
     )
