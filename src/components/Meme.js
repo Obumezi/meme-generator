5575ab24d6c8ac346008965e6f26ffc7d42b6c0e
Obumezi <eziamakaobum@yahoo.co.uk>
2022-04-26 18:27:47 +0100

    implemented image randomizer

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,22 @@
-import {useState} from "react"
+import { useState } from "react"
+import memeData from "../Memedata"
 
 
 export function Meme() {
-   const [ count, setCount] = useState("Hello")
-   console.log(count)
+
+
+    const [meme, memeImage] = useState("")
+
+
+
+
+    function GetMemeImage() {
+        const memesArray = memeData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        meme = memeImage(memesArray[randomNumber].url)
+
+
+    }
 
     return (
         <main className="meme-form">
@@ -12,9 +25,13 @@ export function Meme() {
                 className="form-input" />
             <input type="text" placeholder="bottom text"
                 className="form-input" />
-            <button className="form-button">Get a new meme image</button>
+            <button onClick={GetMemeImage} className="form-button">Get a new meme image</button>
 
-
+            <div className="image-container">
+                <div className="hold">
+                <img src={meme} />
+                </div>
+            </div>
 
         </main>
     )
